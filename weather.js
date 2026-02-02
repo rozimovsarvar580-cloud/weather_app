6e4465d2c1a0e02b55f1c3390573e25d9e183ffd
sarvar <rozimovsarvar580@gmail.com>
2026-02-02 16:37:04 +0300

    v1

---
+++ weather.js
@@ -0,0 +1,9 @@
+// let WHY = 'https://api.openweathermap.org/data/2.5/weather?q=Tashkent   &units=metric&appid=96b947a45d33d7dc1c49af3203966408'
+// const img = document.querySelector('#img')
+//  const getData = async (data) =>{
+//     const respones = await fetch(data)
+//     const info =  await respones.json()
+//     console.log(info)
+//     img.src = `https://openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`
+//  }
+// getData(WHY)
